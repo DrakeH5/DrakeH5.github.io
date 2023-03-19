bc69bf18c3eb9ebdc15cd5960380e725eb9c56d5
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-03-18 23:22:19 -0500

    ground in projects / start of canvas text for projects

--- projectsScene.js
+++ projectsScene.js
@@ -17,21 +17,42 @@ window.addEventListener('resize', () => { //when the page is resized
 })
 
 
-var numberOfProjects = 20
+var material = new THREE.MeshBasicMaterial();
+
+const drawingCanvas = document.getElementById( 'canvas' );
+				const drawingContext = drawingCanvas.getContext( '2d' );
+
+				// draw white background
+
+				drawingContext.fillStyle = 'lightblue';
+				drawingContext.fillRect( 0, 0, 128, 128 );
+
+				// set canvas as material.map (this could be done to any map, bump, displacement etc.)
+
+				material.map = new THREE.CanvasTexture( drawingCanvas );
+
+var numberOfProjects = 10
 var radius = 20;
 for(var i=0; i<numberOfProjects; i++){
-    var geometry = new THREE.BoxGeometry(3, 3, 3); //creates box (width, height, depth)
-    var material = new THREE.MeshLambertMaterial({color: "lightblue"}); //creates material
+    var geometry = new THREE.BoxGeometry(6, 10, 6); //creates box (width, height, depth)
     var mesh = new THREE.Mesh(geometry, material); //creates a mesh out of our box and material
     var theta = (i/numberOfProjects)*360;
     xPos = radius*Math.cos(theta);
     zPos = radius*Math.sin(theta)
-    mesh.position.set(xPos, 0, -zPos);
+    mesh.position.set(xPos, 0, zPos);
     scene.add(mesh); //adds our mesh to the scene
 }
 
 
-var light = new THREE.PointLight(0xFFFFFF, 1, 500); //creates a light (color, intensity, distance)
+var geometry = new THREE.BoxGeometry(100, 1, 100); //creates box (width, height, depth)
+var groundMaterial = new THREE.MeshLambertMaterial({color: "lightgreen"}); //creates material
+var ground = new THREE.Mesh(geometry, groundMaterial); //creates a mesh out of our box and material
+ground.position.y=-8
+scene.add(ground); //adds our mesh to the scene
+
+
+
+var light = new THREE.PointLight(0xFFFFFF, 5, 500); //creates a light (color, intensity, distance)
 light.position.set(0, 0, 0); //sets light position (x, y, z)
 scene.add(light); //add the light to the scene
 
