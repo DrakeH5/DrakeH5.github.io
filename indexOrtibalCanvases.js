6f1a69d863a95aad3d1939a02cb48b9be1bfc694
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-03-26 18:23:00 -0500

    skills orbital

--- indexOrtibalCanvases.js
+++ indexOrtibalCanvases.js
@@ -10,8 +10,20 @@ const orbital1Ctx = orbital1Canvas.getContext( '2d' );
 orbital1Ctx.fillStyle = 'lightblue';
 orbital1Ctx.fillRect( 0, 0, 256, 128);
 orbital1Ctx.fillStyle = 'blue';
-orbital1Ctx.font = "12px Arial";
-orbital1Ctx.fillText("Project 1", 10, 15);
+orbital1Ctx.font = "25px Arial";
+orbital1Ctx.fillText("Skills:", 100, 20);
+orbital1Ctx.fillStyle = 'black';
+orbital1Ctx.font = "15px Arial";
+orbital1Ctx.fillText("•Javascript", 10, 40);
+orbital1Ctx.fillText("•Python", 10, 60);
+orbital1Ctx.fillText("•C++", 10, 80);
+orbital1Ctx.fillText("•Golang", 10, 100);
+orbital1Ctx.fillText("•Web Dev", 10, 120);
+orbital1Ctx.fillText("•Arduino", 160, 40);
+orbital1Ctx.fillText("•Raspbery Pi", 160, 60);
+orbital1Ctx.fillText("•ReactJS", 160, 80);
+orbital1Ctx.fillText("•NodeJs", 160, 100);
+orbital1Ctx.fillText("•MongoDb", 160, 120);
 var orbital1 = new THREE.Mesh(orbitalGeometry, new THREE.MeshBasicMaterial({map: new THREE.CanvasTexture( orbital1Canvas )}));
 ortibals.push(orbital1)
 
